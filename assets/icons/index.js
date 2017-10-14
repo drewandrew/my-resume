dbe80793916d9c7ea45a01419ec4d51776e10799
drewandrew <andrewcdixon673@gmail.com>
2017-10-14 15:22:31 -0400

    add assets for hackvt

---
+++ assets/icons/index.js
@@ -0,0 +1,11 @@
+import * as Beet from './001-food-4.svg';
+import * as Broccolli from './002-food-3.svg';
+import * as Carrot from './006-food-2.svg';
+import * as Bean from './007-food-1.svg';
+import * as Corn from './017-corn.svg';
+import * as Crop from './010-crops.svg';
+import * as Grape from './012-grapes.svg';
+import * as Wheat from './015-wheat.svg';
+import * as Steak from './016-steak.svg'
+
+export { Beet, Broccolli, Carrot, Bean, Corn, Crop, Grape, Wheat, Steak };
